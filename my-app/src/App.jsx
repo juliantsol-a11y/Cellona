961df970c21f893855f0525a35fa75e03b596b9e
juliantsol-a11y <cellonamcjulian@gmail.com>
2026-03-21 08:27:43 +0800

    added comment

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect} from 'react'
+import reactLogo from '/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import  Sample  from './assets/sample'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  //dasasd
   return (
-  <div> <Sample/>
+  <div>
+  <Sample/>
   </div>
 );
-  
-   
-  
 }
-
 export default App
+
